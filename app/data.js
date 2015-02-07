637ee4c5a260c8c5fdf03057b195fb91f2a4afb9
Alex Ngo <alex_ngo@live.com>
2015-02-07 15:01:52 -0800

    it worksssss

--- app/data.js
+++ app/data.js
@@ -1,13 +1,31 @@
-var crimeBaseURL = "https://data.seattle.gov/resource/3k2p-39jp.json?event_clearance_group=ACCIDENT%20INVESTIGATION&event_clearance_code=430"
+var crimeBaseURL = "https://data.seattle.gov/resource/3k2p-39jp.json?initial_type_subgroup="
 
+//list of crime objects
 var crimeList = [];
 
-var populateCrime = function () {
-    $.getJSON(crimeBaseURL, function (data) {
-        for (var i = 0; i < Object.keys(data).length; i++) {
-            console.log(data[i]);
-        }
-    });
+//which crimes are included
+var includedCrimes = ["ASSAULTS", "BURGLARY"];
+
+var populateCrime = function (includedCrimes) {
+    var lst = [];
+    for (var j = 0; j < includedCrimes.length; j++) {
+        var URL = crimeBaseURL + includedCrimes[j];
+        var idx = 0;
+        $.getJSON(URL, function (data) {
+            for (var i = 0; i < Object.keys(data).length; i++) {
+                var crime = {};
+                var curObj = data[i];
+                crime["long"] = curObj["longitude"];
+                crime["lat"] = curObj["latitude"];
+                crime["type"] = includedCrimes[idx];
+                lst.push(crime);
+            }
+            idx++;
+        }).done(function () {
+            crimeList = lst;
+        })
+    }
 }
 
-populateCrime();
+console.log(includedCrimes);
+populateCrime(includedCrimes);
