3319bd00c6b77dc0c2cfb781f5dca2f697caf196
Alex Ngo <alex_ngo@live.com>
2015-02-15 22:02:12 -0800

    sliders, draggable menu, hiding forms

--- app/app.js
+++ app/app.js
@@ -1,3 +1,36 @@
+var safetyPriority;
+var educationPriority;
+var amenPriority;
+$("#test").ionRangeSlider({
+    grid: true,
+    min: 0,
+    max: 10,
+    from: 1,
+    prefix: "Priority: ",
+    onChange: function (data) {
+        safetyPriority = data['from'];
+    }
+});
+$("#test2").ionRangeSlider({
+    grid: true,
+    min: 0,
+    max: 10,
+    from: 5,
+    prefix: "Priority: ",
+    onUpdate: function (data) {
+        educationPriority = data['from'];
+    }
+});
+$("#test3").ionRangeSlider({
+    grid: true,
+    min: 0,
+    max: 10,
+    from: 3,
+    prefix: "Priority: ",
+    onUpdate: function (data) {
+        amenPriority = data['from'];
+    }
+});
 $("#overlaybutton").click(function () {
     $("#mainmenu").show(1000);
     $("#panel").show(1000);
@@ -6,3 +39,19 @@ $("#overlaybutton").click(function () {
 });
 
 $("#overlaybutton").show(800);
+$("#newmap").click(function(){
+
+});
+
+$('#mainmenu').draggable({
+    handle: "#menuheader"
+});
+
+$('#amenitiesoptions').click(function(){
+    if(($('#aform').is(':hidden'))){
+        $('#aform').show(500);
+    }
+    else{
+        $('#aform').hide(500);
+    }
+});
