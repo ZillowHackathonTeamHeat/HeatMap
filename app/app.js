7d7c1b197dd4ac4324b98afcdb0de36a7d0a2b55
Alex Ngo <alex_ngo@live.com>
2015-05-06 21:59:35 -0700

    added new functionality to show and hide option and datepicker

--- app/app.js
+++ app/app.js
@@ -1,43 +1,64 @@
 $("#test").ionRangeSlider({
     grid: true,
     min: 0,
-    max: 10,
+    max: 100,
     from: crimeWeight,
     prefix: "Priority: ",
     onChange: function (data) {
-        safetyPriority = data['from'];
+        crimeWeight = data['from']/10 //crimeList.length;
+        console.log(crimeWeight);
     }
 });
 $("#test2").ionRangeSlider({
     grid: true,
     min: 0,
-    max: 10,
+    max: 100,
     from: schoolWeight,
     prefix: "Priority: ",
-    onUpdate: function (data) {
-        educationPriority = data['from'];
+    onChange: function (data) {
+        schoolWeight = data['from']//schoolList.length;
+        console.log(schoolWeight);
     }
 });
 $("#test3").ionRangeSlider({
     grid: true,
     min: 0,
-    max: 10,
+    max: 100,
     from: amenWeight,
     prefix: "Priority: ",
-    onUpdate: function (data) {
-        amenPriority = data['from'];
+    onChange: function (data) {
+        amenWeight = data['from'] //amenWeight.length;
+        console.log(amenWeight);
     }
 });
-$("#overlaybutton").click(function () {
+//$("#overlaybutton").click(function () {
+$(function(){
+    setTimeout(function(){
+        $('#ajax').show(500);
+    }, 500);
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 5 // Creates a dropdown of 15 years to control year
+    });
+
+});
+
+var $input = $('.datepicker').pickadate()
+var picker = $input.pickadate('picker')
+
+setTimeout(function(){
     $("#mainmenu").show(1000);
     $("#panel").show(1000);
     $("#overlay").hide(1000);
     $("#overlaybutton").hide(1000);
-});
+}, 1000);
 
-$("#overlaybutton").show(800);
 $("#newmapButton").click(function () {
+    $('#ajax').show(350);
+    testData = [];
+    populatePoints();
     setHeatMap();
+    $('#ajax').hide(350);
 });
 
 $('#mainmenu').draggable({
@@ -51,6 +72,26 @@ $('#amenitiesoptions').click(function () {
         $('#aform').hide(500);
     }
 });
+
+$('#educationoptions').click(function () {
+    if (($('#eform').is(':hidden'))) {
+        $('#eform').show(500);
+    } else {
+        $('#eform').hide(500);
+    }
+});
+
+$('#safetyoptions').click(function () {
+    console.log("HI");
+    if (($('#sform').is(':hidden'))) {
+        $('#sform').show(500);
+        $('#dateRange').show(500);
+    } else {
+        $('#sform').hide(500);
+        $('#dateRange').hide(500);
+    }
+});
+
 
 $('.amen').click(function () {
     var amen = $(this).attr('id')
